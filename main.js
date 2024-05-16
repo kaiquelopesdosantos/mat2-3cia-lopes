fd4bdc3a4277c9280ca354a1e0c33ef548baedd5
kaiquelopesdosantos <110171652+kaiquelopesdosantos@users.noreply.github.com>
2024-05-16 16:46:59 -0300

    kkl

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-const botoes
+const botoes = document.querySelectorAll(".botao");
+console.log(botoes)
+for(let i=0; i < botao.length) {
+    console.log(i)
+    
+})
+botao[i].onclick = function(){
+    botao[i].classlist.add("ativo")
+}
+for(let j=0; j<botao.length;j++){
+    
+}
