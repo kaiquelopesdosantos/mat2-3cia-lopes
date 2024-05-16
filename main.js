95e0de828ed000031ccfa484138b48d8d0d6b138
kaiquelopesdosantos <110171652+kaiquelopesdosantos@users.noreply.github.com>
2024-05-16 17:17:33 -0300

    333

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ console.log(botoes)
 for(let i=0; i < botao.length) {
     console.log(i)
     
-})
+}
 botao[i].onclick = function(){
     botao[i].classlist.add("ativo")
 }
 for(let j=0; j<botao.length;j++){
-    
+
 }
