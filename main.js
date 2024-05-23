f9ac74f13f51b04c832921914c74128ce0215bb7
kaiquelopesdosantos <110171652+kaiquelopesdosantos@users.noreply.github.com>
2024-05-23 16:37:33 -0300

    kakaka2004

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
-console.log(botoes)
-for(let i=0; i < botao.length) {
-    console.log(i)
-    
-}
-botao[i].onclick = function(){
-    botao[i].classlist.add("ativo")
-}
-for(let j=0; j<botao.length;j++){
+for(let i=0;i<botoes. length; i++){
+    botoes[i].onclick =function(){
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
 
+        }
+        botoes[i].classList.add("ativo");
+    }
 }
